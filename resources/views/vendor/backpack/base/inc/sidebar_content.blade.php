f8a0b15cfaa4e5efdc40b0d6f91dee756ae48b38
fauzanajipray <fauzan.aji@rectmedia.co.id>
2025-05-28 02:06:15 +0700

    Remove unused migration and request files for outlets, categories, price histories, user outlets, and products

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,8 +1,6 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-question"></i> User</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('outlet') }}"><i class="nav-icon la la-question"></i> Outlet</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user-outlet') }}"><i class="nav-icon la la-question"></i> User Outlet</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('position') }}"><i class="nav-icon la la-question"></i> Positions</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('report') }}"><i class="nav-icon la la-question"></i> Reports</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-users"></i> User</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('position') }}"><i class="nav-icon la la-map-marker"></i> Positions</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('report') }}"><i class="nav-icon la la-file-text-o"></i> Reports</a></li>
