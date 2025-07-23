79e82c0d6cbaa81fdb7f906303e70f5c93fe6a0e
fauzanajipray <fauzan.aji@rectmedia.co.id>
2025-07-23 18:59:19 +0700

    update comment total

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -14,7 +14,7 @@ class CommentController extends Controller
         $comments = Comment::with('user')
             ->where('report_id', $reportId)
             ->orderBy('created_at', 'desc')
-            ->paginate($request->get('per_page', 3));
+            ->paginate($request->get('per_page', 5));
         return response()->json($comments);
     }
 
